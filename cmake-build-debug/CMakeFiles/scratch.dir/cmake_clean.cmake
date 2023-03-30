e7bc9b3a2e404f9c3a33b53517f5ebc8af4d91f1
Tychart <tyler@thechartrands.com>
2023-03-30 15:20:43 -0600

    Update

---
+++ cmake-build-debug/CMakeFiles/scratch.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scratch.dir/AVL.cpp.o"
+  "CMakeFiles/scratch.dir/AVL.cpp.o.d"
+  "CMakeFiles/scratch.dir/scratch.cpp.o"
+  "CMakeFiles/scratch.dir/scratch.cpp.o.d"
+  "scratch"
+  "scratch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scratch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
